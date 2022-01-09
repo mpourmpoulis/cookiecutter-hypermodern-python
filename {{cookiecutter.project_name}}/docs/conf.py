0ab97e8e5c515231e5932490fd70a545b1b27bc9
mpourmpoulis <35875229+mpourmpoulis@users.noreply.github.com>
2022-01-09 18:30:38 +0200

    small change

--- {{cookiecutter.project_name}}/docs/conf.py
+++ {{cookiecutter.project_name}}/docs/conf.py
@@ -1,14 +1,13 @@
 """Sphinx configuration."""
 from datetime import datetime
 
-
 project = "{{cookiecutter.friendly_name}}"
 author = "{{cookiecutter.author}}"
 copyright = f"{datetime.now().year}, {author}"
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
-    "sphinx_click",
+    "sphinx_click"
 ]
 autodoc_typehints = "description"
 html_theme = "furo"
